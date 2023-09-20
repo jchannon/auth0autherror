cc0e1edd751e592e01a2e720ecbaf6f1bae7e79b
Jonathan Channon <jonathan.channon@gmail.com>
2023-09-20 06:33:06 +0100

    initial

---
+++ WebApplication7Auth0/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplication7Auth0.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
